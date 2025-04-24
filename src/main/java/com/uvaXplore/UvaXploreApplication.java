eb2b4ab2ba3d8b845ffa37407051d55cde42769b
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-24 18:13:46 +0530

    initializing project structure

---
+++ src/main/java/com/uvaXplore/UvaXploreApplication.java
@@ -0,0 +1,13 @@
+package com.uvaXplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UvaXploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UvaXploreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uvaXplore/UvaXploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvaXplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UvaXploreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
