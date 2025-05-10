45ab2549a67613677ecfca788222ee5a89b2b59c
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-05-10 15:54:36 +0530

    Developing flask service

---
+++ src/main/java/com/uvaXplore/service/FlaskService.java
@@ -0,0 +1,46 @@
+package com.uvaXplore.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class FlaskService {
+
+    @Value("${flask.service.url}")
+    private String flaskUrl;
+
+    public Map<String, Object>  processPdf(MultipartFile file) throws IOException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        });
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<Map> response = new RestTemplate().postForEntity(
+                flaskUrl + "/api/v1/process",
+                requestEntity,
+                Map.class
+        );
+
+        return response.getBody();
+    }
+}
