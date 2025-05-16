83cf4ed6c66b79e7cfebba813d9c2b2ed1798684
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-05-16 23:36:49 +0530

    change

--- src/main/java/com/uvaXplore/config/SecurityConfig.java
+++ src/main/java/com/uvaXplore/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);  
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 }
